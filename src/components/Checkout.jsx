cf2cfcbb563d182d096f0eb86bb9a44539e5c9e6
JHS96 <jschaper@hotmail.co.za>
2025-08-22 15:23:39 +0200

    Submit order via POST request

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -27,6 +27,17 @@ export default function Checkout() {
     const customerData = Object.fromEntries(fd.entries());
 
     console.log(customerData);
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/JSON' },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -35,9 +46,9 @@ export default function Checkout() {
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label='Full Name' type='text' id='full-name' />
+        <Input label='Full Name' type='text' id='name' />
         <Input label='E-Mail Address' type='email' id='email' />
-        <Input label='Street' type='text' id='text' />
+        <Input label='Street' type='text' id='street' />
         <div className='control-row'>
           <Input label='Postal Code' type='text' id='postal-code' />
           <Input label='City' type='text' id='city' />
